c18fa145abf7bc75cff38715410e1af553ad1746
iplatos <platonoff9168@gmail.com>
2023-04-28 14:59:31 +0300

    Updates

---
+++ precache-manifest.5c3adadbdb875ac259e0250b485da29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b57cc85985d330664c33047f960297e",
+    "url": "/todo_toolkit_thunk/index.html"
+  },
+  {
+    "revision": "3594b3473f243c13bf75",
+    "url": "/todo_toolkit_thunk/static/css/main.6da83bb5.chunk.css"
+  },
+  {
+    "revision": "108c687625ace05021cd",
+    "url": "/todo_toolkit_thunk/static/js/2.4a6d0ca1.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo_toolkit_thunk/static/js/2.4a6d0ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3594b3473f243c13bf75",
+    "url": "/todo_toolkit_thunk/static/js/main.c5f65ddc.chunk.js"
+  },
+  {
+    "revision": "bf01095805fb2323faff",
+    "url": "/todo_toolkit_thunk/static/js/runtime-main.2cd352af.js"
+  }
+]);
