aaad193258ab6d830c4077eff7d91af17e248a71
iplatos <platonoff9168@gmail.com>
2023-04-29 21:29:17 +0300

    fix: test fix CORS

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -4,7 +4,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function (app:any){
     app.use(
         proxy("auth/me ",{
-            target:"https://social-network.samuraijs.com",
+            target:"https://social-network.samuraijs.com/api/1.1",
             secure:false,
             changeOrigin:true
         })
